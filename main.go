9dd43caa29d526c3818c533ed8c7f6c0c6b1a1f6
Takenouchi Keita <t.keita.osaka@gmail.com>
2018-11-23 01:09:10 +0900

    Implement simple algorithm (nearest next).

--- main.go
+++ main.go
@@ -58,6 +58,58 @@ func (p *path) distance() float64 {
 	return sum
 }
 
+type pointPool struct {
+	points []*point
+}
+
+func (pp *pointPool) addPoint(pt *point) {
+	pp.points = append(pp.points, pt)
+}
+
+func (pp *pointPool) removeAt(i int) *point {
+	pt := pp.points[i]
+	pp.points = append(pp.points[:i], pp.points[i+1:]...)
+	return pt
+}
+
+func (pp *pointPool) removeById(id int) *point {
+	targetIndex := -1
+	for i, pt := range pp.points {
+		if pt.id == id {
+			targetIndex = i
+			break
+		}
+	}
+	return pp.removeAt(targetIndex)
+}
+
+func (pp *pointPool) size() int {
+	return len(pp.points)
+}
+
+func (pp *pointPool) isEmpty() bool {
+	return len(pp.points) == 0
+}
+
+func (pp *pointPool) nearest(target *point) *point {
+	var nearestPt *point
+	minDist := math.MaxFloat64
+	for _, pt := range pp.points {
+		d := dist(*target, *pt)
+		if d < minDist {
+			minDist = d
+			nearestPt = pt
+		}
+	}
+	return nearestPt
+}
+
+func newPointPool() pointPool {
+	return pointPool{
+		points: make([]*point, 0),
+	}
+}
+
 func isPrime(n int) bool {
 	limit := math.Floor(math.Sqrt(float64(n)))
 	for i := 2; i < int(limit); i++ {
@@ -69,29 +121,65 @@ func isPrime(n int) bool {
 }
 
 func main() {
-	file, err := os.Open("./cities.csv")
-	//file, err := os.Open("./small.csv")
+	rfile, err := os.Open("./cities.csv")
+	//rfile, err := os.Open("./small.csv")
+	defer rfile.Close()
 	if err != nil {
 		panic("fine not found.")
 	}
-	reader := csv.NewReader(bufio.NewReader(file))
+	reader := csv.NewReader(bufio.NewReader(rfile))
 
-	path := newPath()
+	// read points as pool from a file.
+	pool := newPointPool()
 	for i := 0; ; i++ {
 		line, err := reader.Read()
 		if err == io.EOF {
 			break
 		}
+
+		id, e0 := strconv.Atoi(line[0])
 		x, e1 := strconv.ParseFloat(line[1], 64)
 		y, e2 := strconv.ParseFloat(line[2], 64)
-		if e1 != nil || e2 != nil {
+		if e0 != nil || e1 != nil || e2 != nil {
 			continue
 		}
-		pt := newPoint(i, x, y)
-		path.addPoint(pt)
+		pt := newPoint(id, x, y)
+		pool.addPoint(pt)
 	}
 
+	// calculate a path.
+	path := nearestNextAlgorithm(pool)
 	dist := path.distance()
 	fmt.Printf("dist %f", dist)
 
+	// write the result as csv.
+	wfile, err := os.Create("./result.csv")
+	defer wfile.Close()
+
+	buf := bufio.NewWriter(wfile)
+	buf.WriteString("Path, X, Y\n")
+	for _, pt := range path.points {
+		line := fmt.Sprintf("%d, %f, %f\n", pt.id, pt.x, pt.y)
+		buf.WriteString(line)
+	}
+
+	start := path.points[0]
+	line := fmt.Sprintf("%d, %f, %f\n", start.id, start.x, start.y)
+	buf.WriteString(line)
+
+	buf.Flush()
+}
+
+func nearestNextAlgorithm(pool pointPool) *path {
+	path := newPath()
+
+	currentPoint := pool.removeAt(0)
+	path.addPoint(currentPoint)
+	for !pool.isEmpty() {
+		nextPt := pool.nearest(currentPoint)
+		pool.removeById(nextPt.id)
+		path.addPoint(nextPt)
+		currentPoint = nextPt
+	}
+	return path
 }
